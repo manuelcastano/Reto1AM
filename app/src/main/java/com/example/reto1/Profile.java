9f5ad9d2f15e463227021a7546c098b3fd5e4765
Andres Mayor <andresmayor03@gmail.com>
2021-10-04 15:01:30 -0500

    fragmento perfil terminado

--- app/src/main/java/com/example/reto1/Profile.java
+++ app/src/main/java/com/example/reto1/Profile.java
@@ -1,20 +1,21 @@
 package com.example.reto1;
 
-import android.widget.ImageView;
+import android.net.Uri;
 
 public class Profile {
 
     private String title;
-    //private ImageView image;
+    private Uri uri;
     private String description;
 
 
     public Profile(){
 
     }
-    public Profile(String title, String description) {
+    public Profile(String title, String description,Uri uri) {
         this.title = title;
         this.description = description;
+        this.uri = uri;
     }
 
     public String getTitle() {
@@ -31,5 +32,12 @@ public class Profile {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+    public Uri getUri() {
+        return uri;
+    }
+
+    public void setUri(Uri uri) {
+        this.uri = uri;
     }
 }
